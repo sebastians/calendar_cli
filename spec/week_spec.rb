17f7ed15d88a370efafd059c1feffdb67b1a375f
Sebastian Sogamoso <sebasoga@gmail.com>
2021-10-01 15:39:41 -0500

    Improve tests for `Week`
    Split tests between `#to_a` and `to_s`.

--- spec/week_spec.rb
+++ spec/week_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Week do
   subject { described_class.new(date: date, calendar_month: month) }
 
   describe "to_a" do
-    it "contains seven Day instances" do
+    it "returns an Array containing seven Day instances" do
       expect(subject.to_a[0]).to be_a Day
       expect(subject.to_a[1]).to be_a Day
       expect(subject.to_a[2]).to be_a Day
@@ -17,20 +17,32 @@ RSpec.describe Week do
       expect(subject.to_a[5]).to be_a Day
       expect(subject.to_a[6]).to be_a Day
     end
+  end
+
+  describe "to_s" do
+    it "returns an Array containing seven String instances" do
+      expect(subject.to_s[0]).to be_a String
+      expect(subject.to_s[1]).to be_a String
+      expect(subject.to_s[2]).to be_a String
+      expect(subject.to_s[3]).to be_a String
+      expect(subject.to_s[4]).to be_a String
+      expect(subject.to_s[5]).to be_a String
+      expect(subject.to_s[6]).to be_a String
+    end
 
     it "starts with the given date" do
-      expect(subject.to_a.first.to_s).to eq "5"
+      expect(subject.to_s.first).to eq "5"
     end
 
     it "ends with a date 6 days later" do
-      expect(subject.to_a.last.to_s).to eq "11"
+      expect(subject.to_s.last).to eq "11"
     end
 
     context "when the given date is not a Sunday" do
       let(:date) { Date.parse("2021-12-03") }
 
       it "starts with the previous Sunday" do
-        expect(subject.to_a.first.to_s).to eq ""
+        expect(subject.to_s.first).to eq ""
       end
 
       it "ends with a date 6 days later" do
